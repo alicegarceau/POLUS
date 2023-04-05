68da3abadf715fa36e0055428223af66d92fcd7e
Frédérik Desaulniers <91970175+FredDesaulniers@users.noreply.github.com>
2023-04-05 11:54:10 -0400

    Update stepperZ.cpp
    Ajout en-tête et traduction / ajout de commentaires EN FRANCAIS
    
    Signed-off-by: Frédérik Desaulniers <91970175+FredDesaulniers@users.noreply.github.com>

--- Code_OpenCR/stepperZ.cpp
+++ Code_OpenCR/stepperZ.cpp
@@ -1,16 +1,26 @@
+/*
+Projet: POLUS
+Description: Méthode principale (main) qui gère la communication et les actions du robot à haut niveau
+Auteurs: Frédérik Desaulniers – DESF3105
+         Pierre-Olivier Dupont – DUPP2408
+         Alice Garceau – GARA2507
+         Enrick Hébert – HEBE2701
+         Émile Michaud – MICE1602
+*/
+
 #include "stepperZ.hpp"
 
 //Declare variables for functions
-const int MAX_MM = 100; // Soft limit, max height allowed relative to home pos (lim switch)
-const int MIN_MM =   5; // Soft limit, min height allowed relative to home pos (lim switch)
-static int currPosmm; // Current position in mm
+const int MAX_MM = 100; // Limit logicielle, hauteur maximale permise par rapport à la position d'origine (interrupteur de fin de course)
+const int MIN_MM =   5; // Limit logicielle, hauteur minimale permise par rapport à la position d'origine (interrupteur de fin de course)
+static int currPosmm; // Position actuelle en mm
 const float OffsetCoeff = 0.0025;
 
 int oneTime = 1;
 
 
-
-//Main loop
+// Pour deboguage
+//Main loop 
 //void loop() {
 //  while( oneTime ) {
 //    homeZ();
@@ -22,6 +32,7 @@ int oneTime = 1;
 //}
 
 void setupStepper()
+  // À mettre dans la fonction setup du fichier principal
 {
   pinMode(PIN_STP, OUTPUT);
   pinMode(PIN_DIR, OUTPUT);
@@ -36,8 +47,8 @@ void setupStepper()
   digitalWrite(PIN_EN, HIGH);
 }
 
-//Reset Easy Driver pins to default states
 void resetEDPins()
+// Remettre les broches du Easy Driver à leur état de départ (au setup)
 {
   digitalWrite(PIN_STP, LOW);
   digitalWrite(PIN_DIR, LOW);
@@ -47,6 +58,7 @@ void resetEDPins()
 }
 
 void homeZ()
+// Retourner à la position d'origine en Z. Descend jusqu'à toucher l'interrupteur de fin de course, puis remonte de MIN_MM.
 {
   digitalWrite(PIN_MS1, HIGH);
   digitalWrite(PIN_MS2, HIGH);
@@ -54,9 +66,9 @@ void homeZ()
   digitalWrite(PIN_DIR, LOW);
   while( digitalRead(PIN_SW) != false )
   {
-    digitalWrite(PIN_STP,HIGH); //Trigger one step forward
-    delayMicroseconds(500);
-    digitalWrite(PIN_STP,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(PIN_STP,HIGH); // Déclencher un pas
+    delayMicroseconds(500);     // Attendre pour le mouvement mécanique du rotor. Delais plus long vs mouvement pour avancer lentement
+    digitalWrite(PIN_STP,LOW); // Remettre la broche à l'état BAS, pour pouvoir la réactiver
     delayMicroseconds(500);
   }
   currPosmm = 0;
@@ -73,36 +85,29 @@ float updateZOffset(float yPos)
 // 1/8th microstep foward mode function
 void stepperGoToPos(float goalmm) // goalmm, 
 {
-  /*
-  float ZOffset;
-  ZOffset = updateZOffset(yPos);
-  goalmm += ZOffset; 
-  */
+// Aller à une position donnée en mm par rapport à la position d'origine. Utilise le mode micro-stepping à 1/8 de pas.
 
-  // Todo : add software limits
+
   float displacementmm = goalmm - currPosmm; 
   currPosmm += displacementmm;
-  int nMicroSteps = abs((displacementmm / MM_PER_TURN) * STEPS_PER_TURN * MICRO_STEPS); // Take absolue value
+  int nMicroSteps = abs((displacementmm / MM_PER_TURN) * STEPS_PER_TURN * MICRO_STEPS); // Calcul du nombre de micro-pas requis pour atteindre la destination
   int direction;
   if (displacementmm < 0) {
     direction = LOW;
   } else {
     direction = HIGH;
   }
-  digitalWrite(PIN_DIR, direction); // Pull direction pin low to move "forward". Make LOW/HIGH depend on sign of distmm
-  digitalWrite(PIN_MS1, HIGH); //Pull PIN_MS1, and PIN_MS2 high to set logic to 1/8th microstep resolution
+  digitalWrite(PIN_DIR, direction); // Établi l'état BAS ou HAUT de la broche de contrôle de la direction. Dépend du signe de la cible (distmm)
+  digitalWrite(PIN_MS1, HIGH); // Établi l'état des broches PIN_MS1 et PIN_MS2 à HAUT pour que le mode micro-stepping soit à 1/8 de pas.
   digitalWrite(PIN_MS2, HIGH);
-  digitalWrite(PIN_EN, LOW); // Pull Enable pin low (logic high) to allow
-  //Serial.println(nMicroSteps);
+  digitalWrite(PIN_EN, LOW); // Établi l'état BAS de la broche de désactivation pour permettre le mouvement.
 
-  for(int x= 1; x<nMicroSteps; x++)  //Loop the forward stepping enough times for motion to be visible
+  for(int x= 1; x<nMicroSteps; x++)  
   {
-    digitalWrite(PIN_STP,HIGH); //Trigger one step forward
-    delayMicroseconds(100);
-    digitalWrite(PIN_STP,LOW); //Pull step pin low so it can be triggered again
+    digitalWrite(PIN_STP,HIGH); // Déclencher un pas
+    delayMicroseconds(100);     // Délais minimal de 100 microsecondes pour laisser le temps au rotor d'arriver à sa position de micro-pas
+    digitalWrite(PIN_STP,LOW); // Remettre la broche à l'état BAS, pour pouvoir la réactiver
     delayMicroseconds(100);
   }
-
-  //Serial.println(currPosmm);
 
 }
